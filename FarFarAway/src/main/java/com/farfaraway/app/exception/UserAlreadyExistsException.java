e5dc650cf650e4129b103a81ddbb42690d472321
Roger <49531890+rogergibertrovira@users.noreply.github.com>
2023-05-22 19:54:26 +0200

    Updates

--- FarFarAway/src/main/java/com/farfaraway/app/exception/UserAlreadyExistsException.java
+++ FarFarAway/src/main/java/com/farfaraway/app/exception/UserAlreadyExistsException.java
@@ -1,9 +1,6 @@
 package com.farfaraway.app.exception;
 
 public class UserAlreadyExistsException extends RuntimeException {
-    /**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 	public UserAlreadyExistsException(String message) {
--- FarFarAway/src/main/java/com/farfaraway/app/jwt/JWTAuthenticationRequest.java
+++ FarFarAway/src/main/java/com/farfaraway/app/jwt/JWTAuthenticationRequest.java
@@ -2,10 +2,6 @@ package com.farfaraway.app.jwt;
 
 import lombok.Data;
 
-/**
- * @author Jose Marin
- */
-
 @Data
 public class JWTAuthenticationRequest {
     private String userName;
