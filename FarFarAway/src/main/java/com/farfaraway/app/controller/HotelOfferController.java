7c5e371cc589ce6eb73f59e2fe4a61f8a723f3fd
Roger <49531890+rogergibertrovira@users.noreply.github.com>
2023-05-26 12:23:46 +0200

    Updates controllers traveloffer hoteloffer

--- FarFarAway/src/main/java/com/farfaraway/app/controller/HotelOfferController.java
+++ FarFarAway/src/main/java/com/farfaraway/app/controller/HotelOfferController.java
@@ -4,7 +4,6 @@ import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,7 +16,6 @@ import com.farfaraway.app.dto.HotelOffer;
 import com.farfaraway.app.service.HotelOfferService;
 
 @RestController
-@CrossOrigin(origins = "*")
 public class HotelOfferController {
 
 	@Autowired
--- FarFarAway/src/main/java/com/farfaraway/app/controller/TravelOfferController.java
+++ FarFarAway/src/main/java/com/farfaraway/app/controller/TravelOfferController.java
@@ -4,7 +4,6 @@ import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,7 +16,6 @@ import com.farfaraway.app.dto.TravelOffer;
 import com.farfaraway.app.service.TravelOfferService;
 
 @RestController
-@CrossOrigin(origins = "*")
 public class TravelOfferController {
 
 	@Autowired
