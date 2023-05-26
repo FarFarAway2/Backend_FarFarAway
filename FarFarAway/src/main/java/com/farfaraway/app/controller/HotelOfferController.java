b89ab5df02b18796370bc6e0a22c5525f5696b15
Roger <49531890+rogergibertrovira@users.noreply.github.com>
2023-05-26 10:33:13 +0200

    Update HotelOfferController.java

--- FarFarAway/src/main/java/com/farfaraway/app/controller/HotelOfferController.java
+++ FarFarAway/src/main/java/com/farfaraway/app/controller/HotelOfferController.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import com.farfaraway.app.dto.HotelOffer;
 import com.farfaraway.app.service.HotelOfferService;
 
 @RestController
+@CrossOrigin(origins = "https://main.df7xyjdxqxvgb.amplifyapp.com, localhost:4200")
 public class HotelOfferController {
 
 	@Autowired
