612dd8fb8595b1390566d0b5c065535e17364f78
Roger <49531890+rogergibertrovira@users.noreply.github.com>
2023-05-26 12:35:18 +0200

    Updates controllers

--- FarFarAway/src/main/java/com/farfaraway/app/controller/TravelOfferController.java
+++ FarFarAway/src/main/java/com/farfaraway/app/controller/TravelOfferController.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import com.farfaraway.app.dto.TravelOffer;
 import com.farfaraway.app.service.TravelOfferService;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class TravelOfferController {
 
 	@Autowired
