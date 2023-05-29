5ccbae6cb8c75fb2f7b82302f41e2a1e5aeb059e
Roger <49531890+rogergibertrovira@users.noreply.github.com>
2023-05-29 12:26:59 +0200

    Update FarFarAwaySecurityConfig.java

--- FarFarAway/src/main/java/com/farfaraway/app/security/FarFarAwaySecurityConfig.java
+++ FarFarAway/src/main/java/com/farfaraway/app/security/FarFarAwaySecurityConfig.java
@@ -54,7 +54,8 @@ public class FarFarAwaySecurityConfig {
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		return http.csrf().disable().authorizeHttpRequests().requestMatchers(UN_SECURED_URLs).permitAll().and()
+		return http.csrf().disable().authorizeHttpRequests().requestMatchers(UN_SECURED_URLs).permitAll().and().
+				cors().and()
 				.authorizeHttpRequests().requestMatchers(SECURED_URLs).hasAuthority("ADMIN").and()
 				.authorizeHttpRequests().requestMatchers(USER_SECURED_URLs).hasAuthority("USER").and()
 				.authorizeHttpRequests().requestMatchers(COMPANY_SECURED_URLs).hasAuthority("COMPANY").anyRequest()
